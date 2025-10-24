e284a9cf0736564170c5e290dc88a8bd6c0976b1
Tinkerrer <lol@kek.com>
2025-10-24 12:23:23 +0300

    Removing random padding from training.py

--- src/training/training.py
+++ src/training/training.py
@@ -7,7 +7,9 @@ from pathlib import Path
 
 from model import BidirectionalRNNClassifier
 
-RANDOM_PADDING = False
+# Random padding between functions FLAG
+RANDOM_PADDING_ON_TRAIN = True
+RANDOM_PADDING_ON_VALUE = True
 
 def main(idb_dir_path):
     idb_data = []
@@ -16,6 +18,7 @@ def main(idb_dir_path):
     for root, dirs, files in os.walk(idb_dir_path):
         for filename in files:
 
+            # Searching for IDA pro bases
             file_path = Path(root) / filename
             if file_path.suffix.lower() not in ('.idb', '.i64'):
                 continue
@@ -25,7 +28,6 @@ def main(idb_dir_path):
             with idb.from_file(file_path) as db:
                 api = idb.IDAPython(db)
 
-                # Creating byte sequences with "start func" property
                 for seg_start in api.idautils.Segments():
                     seg_end = api.idc.SegEnd(seg_start)
 
@@ -35,34 +37,37 @@ def main(idb_dir_path):
 
                     property_seq = bytearray([0] * (seg_end - seg_start))
 
-                    # Creating set from func start addresses
+                    # Creating property sequence from func start addresses
                     start_func_addresses = api.idautils.Functions(seg_start, seg_end - 1)
 
                     for addr in start_func_addresses:
                         property_seq[addr - seg_start] = 1
 
+                    # Creating byte sequence with get_bytes()
                     byte_seq = api.idaapi.get_bytes(seg_start, seg_end - seg_start)
-
-                    # Without Random padding between functions
-                    if not RANDOM_PADDING:
-                        idb_data.append(byte_seq)
-                        idb_func_property.append(property_seq)
-                        continue
-
-                    # With Random padding between functions
-                    bytearray_seq = bytearray(byte_seq)
-
-                    if api.idc.GetSegmentAttr(seg_start, api.idc.SEGATTR_TYPE) == 2:
-                        print(bytes(bytearray_seq).count(0))
-
-                        for index in range(len(bytearray_seq)):
-                            if bytearray_seq[index] == 0x00 and (api.ida_funcs.get_func(seg_start + iter) is None):
-                                bytearray_seq[index] = random.randint(0, 255)
-
-                    idb_data.append(bytes(bytearray_seq))
+                    idb_data.append(byte_seq)
                     idb_func_property.append(property_seq)
 
-    model = BidirectionalRNNClassifier()
+                    # # Saving sequences
+                    # if not RANDOM_PADDING:
+                    #     idb_data.append(byte_seq)
+                    #     idb_func_property.append(property_seq)
+                    #     continue
+                    #
+                    # # Replacing zeros between functions with random paddings and saving sequences
+                    # bytearray_seq = bytearray(byte_seq)
+                    #
+                    # if api.idc.GetSegmentAttr(seg_start, api.idc.SEGATTR_TYPE) == 2:
+                    #     print(bytes(bytearray_seq).count(0))
+                    #
+                    #     for index in range(len(bytearray_seq)):
+                    #         if bytearray_seq[index] == 0x00 and (api.ida_funcs.get_func(seg_start + index) is None):
+                    #             bytearray_seq[index] = random.randint(0, 255)
+                    #
+                    # idb_data.append(bytes(bytearray_seq))
+                    # idb_func_property.append(property_seq)
+
+    model = BidirectionalRNNClassifier(random_padding_on_train=RANDOM_PADDING_ON_TRAIN, random_padding_on_val=RANDOM_PADDING_ON_VALUE)
 
     print(f"Total functions number: {sum(sum(seq) for seq in idb_func_property)}")
     print(f"Total bytes number: {sum(len(seq) for seq in idb_func_property)}")
@@ -71,7 +76,15 @@ def main(idb_dir_path):
     print(model.summary())
     model.train(x_data, y_data)
 
-    model.save_model("find_func_start_model.h5")
+    saved_model_file_name = "find_func_start_model.h5"
+
+    if RANDOM_PADDING_ON_TRAIN:
+        saved_model_file_name = "train_rand_pad_" + saved_model_file_name
+
+    if RANDOM_PADDING_ON_VALUE:
+        saved_model_file_name = "value_rand_pad_" + saved_model_file_name
+
+    model.save_model(saved_model_file_name)
 
 
 if __name__ == "__main__":
